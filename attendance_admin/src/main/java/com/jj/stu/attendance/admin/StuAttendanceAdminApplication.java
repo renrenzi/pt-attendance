8d086237d162bca3b566472535d5ea9b0ed680f0
zhangjunjie <486084544@qq.com>
2022-11-02 16:10:32 +0800

    整体框架搭建 pom 文件修改

---
+++ attendance_admin/src/main/java/com/jj/stu/attendance/admin/StuAttendanceAdminApplication.java
@@ -0,0 +1,19 @@
+package com.jj.stu.attendance.admin;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * admin 主启动类
+ *
+ * @author 任人子
+ * @date 2022/11/2  - {TIME}
+ */
+
+@SpringBootApplication
+public class StuAttendanceAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StuAttendanceAdminApplication.class, args);
+    }
+
+}
