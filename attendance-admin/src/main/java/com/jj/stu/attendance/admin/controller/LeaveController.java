4cf7568b3a0481250e38a89c1afddd859d33d88b
renrenzi <Zinner2@163.com>
2022-12-07 21:34:01 +0800

    请假模块

--- attendance-admin/src/main/java/com/jj/stu/attendance/admin/controller/LeaveController.java
+++ attendance-admin/src/main/java/com/jj/stu/attendance/admin/controller/LeaveController.java
@@ -3,17 +3,21 @@ package com.jj.stu.attendance.admin.controller;
 import com.jj.stu.attendance.admin.service.LeaveService;
 import com.jj.stu.attendance.base.basic.Result;
 import com.jj.stu.attendance.base.basic.ResultGenerator;
+import com.jj.stu.attendance.base.exception.ApiException;
 import com.jj.stu.attendance.base.util.ValidateUtil;
 import com.jj.stu.attendance.dao.request.leave.PageLeaveRequest;
 import com.jj.stu.attendance.dao.response.leave.PageLeaveResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * 离开控制器
@@ -28,6 +32,15 @@ public class LeaveController {
 
     @Resource
     private LeaveService leaveService;
+
+    @ApiOperation("批量删除请假列表")
+    @PostMapping("/batch/delete/leave/list")
+    public Result<String> batchDeleteLeaveList(@RequestBody List<Integer> leaveIds){
+        if(CollectionUtils.isEmpty(leaveIds)){
+            throw new ApiException("leaveIds 为空");
+        }
+        return ResultGenerator.getResultByOk("删除成功");
+    }
     @ApiOperation("分页获取请假列表")
     @PostMapping("/page/leave/list")
     public Result<PageLeaveResponse> pageLeaveList(@RequestBody PageLeaveRequest request){
