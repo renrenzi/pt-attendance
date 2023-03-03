2f2451bdcf16b4932ea2469cae959ebf0c857d82
zhangjunjie <486084544@qq.com>
2023-03-03 16:53:00 +0800

    update file

--- attendance-task/src/main/java/com/jj/stu/attendance/task/listener/ExportTaskListener.java
+++ attendance-task/src/main/java/com/jj/stu/attendance/task/listener/ExportTaskListener.java
@@ -1,6 +1,7 @@
 package com.jj.stu.attendance.task.listener;
 
 import com.jj.stu.attendance.task.event.ExportTaskEvent;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -9,16 +10,17 @@ import org.springframework.stereotype.Component;
  * @description
  */
 @Component
+@Slf4j
 public class ExportTaskListener {
 
     @EventListener
     public void onApplicationListener(ExportTaskEvent event) {
-        System.out.println("export listener start ---");
+        log.info("export listener start ----------------");
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        System.out.println("export listener end ---");
+        log.info("export listener end ----------------");
     }
 }
